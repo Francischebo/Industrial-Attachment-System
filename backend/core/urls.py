2927d61c9bf255f295f5cdb72955ca25e7bdf8c2
Francischebo <fividah@gmail.com>
2026-04-23 15:45:03 +0300

    Security Update: Enforce strict ProtectedMediaView by removing public media route, and add sample ICT vacancy

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.conf import settings
-from django.views.static import serve
+from django.urls import path, include
 
 from django.http import JsonResponse
 
@@ -16,5 +14,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
     path("api/jobs/", include("jobs.urls")),
-    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
 ]
