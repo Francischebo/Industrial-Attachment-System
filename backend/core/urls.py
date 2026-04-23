43f1262c269177fae327107474145b8db3ab9f37
Francischebo <fividah@gmail.com>
2026-04-23 09:49:09 +0300

    fix: resolve 404 not found on backend root url

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,7 +3,16 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
 
+from django.http import JsonResponse
+
+def api_root(request):
+    return JsonResponse({
+        "status": "success",
+        "message": "Industrial Attachment System API is running smoothly."
+    })
+
 urlpatterns = [
+    path("", api_root),
     path("admin/", admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
     path("api/careers/", include("jobs.urls")),
